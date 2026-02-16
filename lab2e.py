602d9e17d0fda50b86338821adf8852b28c0c19c
Pouya Mostafavi <pmostafavi@myseneca.ca>
2026-02-16 02:43:15 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Pouya Mostafavi
+# AuthorID: pmostafavi
+# Date Created: 2026/02/16
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+if timer == 0:
+    print('blast off!')
